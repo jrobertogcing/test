a38644dce91d2e4edd55bc75f6f3c6307a9f3d60
Roberto Gonzalez <robergonzalez@ebay.com>
2019-10-21 15:27:04 -0700

    Initial Commit

---
+++ navIssue/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  navIssue
+//
+//  Created by Roberto Gonzalez on 10/21/19.
+//  Copyright © 2019 Roberto Gonzalez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
